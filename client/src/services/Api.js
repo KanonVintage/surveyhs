7ab0556c35d7c8544fd426d1786f10d68a00a788
Julio Guilindro <julio.guilindro@gmail.com>
2019-01-06 02:06:51 -0500

    Added support for cloud api

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default() => {
   return axios.create({
-    baseURL: `http://localhost:8081`
+    baseURL: `http://ec2-54-88-80-204.compute-1.amazonaws.com:8081`
   })
 }
